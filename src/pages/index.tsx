440deee4b506be75a855411dfb5b138a73c3ca17
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-08-05 18:43:15 -0300

    test: display image pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, NextPage } from "next";
+import Image from "next/image"
 import api from "../services/api";
 
 export interface IPokeApi {
@@ -12,6 +13,7 @@ export interface IPokemonInfo {
   name: string;
   url: string;
   id: number;
+  image: string
 }
 
 interface IProps {
@@ -23,7 +25,11 @@ const Home: NextPage<IProps> = ({ pokemons }) => {
     <>
       <ul>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <li key={pokemon.id}>
+            <Image src={pokemon.image} alt={pokemon.name} unoptimized width="100" height="100"/>
+            {pokemon.id}.{" "}
+            {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
+          </li>
         ))}
       </ul>
     </>
@@ -35,10 +41,13 @@ export default Home;
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get<IPokeApi>("/pokemon?limit=151");
 
-  const pokemons: IPokemonInfo[] = data.results.map((pokemon, i) => ({
-    ...pokemon,
-    id: i + 1,
-  }));
+  const pokemons: IPokemonInfo[] = data.results.map((pokemon, i) => {
+    const id = i + 1
+    return {
+      ...pokemon,
+      id,
+    };
+  });
 
   return {
     props: {
